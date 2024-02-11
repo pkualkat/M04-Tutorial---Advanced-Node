d0550e325e00c61452a86d36726b0698d9e88469
pkualkat <pkualkat@ivytech.edu>
2024-02-10 19:55:16 -0500

    public

--- server.js
+++ server.js
@@ -2,27 +2,13 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
+ 
   console.log(req.url);
 
-  // set header content type
+
   res.setHeader('Content-Type', 'text/html');
 
-  // res.write('<p>hello, ninjas</p>');
-  // res.write('<p>hello again, ninjas</p>');
-  // res.end();
 
-  // send html file
-  // fs.readFile('./views/index.html', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.end();
-  //   }
-  //   //res.write(data);
-  //   res.end(data);
-  // });
-
-  // routing
   let path = './views/';
   switch(req.url) {
     case '/':
@@ -43,20 +29,19 @@ const server = http.createServer((req, res) => {
       res.statusCode = 404;
   }
 
-  // send html
+  
   fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
     }
-    //res.write(data);
+    
     res.end(data);
   });
 
 
 });
 
-// localhost is the default value for 2nd argument
 server.listen(3000, 'localhost', () => {
   console.log('listening for requests on port 3000');
 });
